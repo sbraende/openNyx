7de60e63f76f34810f927b6a6f23d01a80d78efb
sbraende <sebastian.braende@gmail.com>
2024-06-08 21:57:56 +0200

    Player cloudstorage prep

--- index.js
+++ index.js
@@ -11,64 +11,109 @@ gameTickBtn.addEventListener("click", function() {
 
 // GAME LOGIC
 const currentGame = "season1"
-const currentPlayer = "Seb"
+const currentPlayer = "Iris"
 
 class game {
     constructor(name) {
         this.name = name
         this.startDate = Date.now()
-        this.players = []
+        this.players = {} // Updated to object, will contain player name(later id and highscore)
     }
 }
 
-// Create season1Game, if not in cloud
+// Create current game, if not in cloud
 if (localStorage.getItem(currentGame) === null) {
     const currentGameObj = new game(currentGame)
     cloudSave(currentGameObj.name, currentGameObj)
     console.log("Created save file")
 }
 
-// Player
+// Player fuctions
 class player {
     constructor(name) {
-        this.id = Math.floor(Math.random() * 1000000)
+        // this.id = Math.floor(Math.random() * 1000000)
         this.name = name
         this.startDate = Date.now()
+        this.lastAction = Date.now()
 
         this.metal = 50
         this.crystal = 0
         this.energy = 0
+
+        this.metalMultiplier = 1
+        this.crystalMultiplier = 0.1
     }
 }
 
 function createPlayer(name) {
     const playerObj = new player(name)
-    const gameObj = cloudGetObj(currentGame)
 
-    gameObj.players.push(playerObj)
+    // Create player main game object
+    const playerGameObj = {
+        playerName: name,
+        highscore: 100
+    }
+    const gameObj = cloudGetObj(currentGame)    
+    gameObj.players[name] = playerGameObj
     cloudSave(currentGame, gameObj)
+
+    // Create player cloud profile
+    cloudSave(playerObj.name, playerObj)
 }
 
+// createPlayer("Iris")
 
-// Resources 
-class metal {
-    constructor() {
-        this.value = 0
-        this.multiplier = 1
+function giftMetal(playerName){
+    const playerObj = cloudGetObj(playerName)
+
+    if (playerObj === null) {
+        console.log("No player found")
+    } else {
+        playerObj.metal += 500
+        playerObj.lastAction = Date.now()
+        cloudSave(playerObj.name, playerObj)
     }
 }
 
-// Metal mines levels
-const metalMine = {
-    level1 : buildCost; 10 metal, 5 crystal, 
-             production_type; metal
-             production_ammount; 50 
-             energy_consumption; 10
-    level2 : buildCost; 50 metal, 25 crystal, 
-             production_type; metal
-             production_ammount; 100 
-             energy_consumption; 25
+function buildMine(playerName){
+    const playerObj = cloudGetObj(playerName)
+    if (playerObj === null) {
+        console.log("No player found")
+    } else {
+        console.log("Building mine costing 1000 metal")
+        playerObj.metal -= 1000
+        playerObj.crystal -= 10 
+        playerObj.metalMultiplier += 1
+        playerObj.lastAction = Date.now()
+        cloudSave(playerObj.name, playerObj)
+    }
 }
+
+function resourceTick(playerName) {
+    const playerObj = cloudGetObj(playerName)
+
+    const timeDifference = Math.floor((Date.now() - playerObj.lastAction)/1000)
+
+    // Give metal // BUG if tick is quicker than 10 seconds, crystal will be floored
+    playerObj.metal += Math.floor(timeDifference * playerObj.metalMultiplier)
+    playerObj.crystal += Math.floor(timeDifference * playerObj.crystalMultiplier)
+
+    playerObj.lastAction = Date.now()
+    cloudSave(playerObj.name, playerObj)
+}
+
+
+// Metal mines levels
+// const metalMine = {
+//     level1 : buildCost; 10 metal, 5 crystal, 
+//              production_type; metal
+//              production_ammount; 50 
+//              energy_consumption; 10
+//     level2 : buildCost; 50 metal, 25 crystal, 
+//              production_type; metal
+//              production_ammount; 100 
+//              energy_consumption; 25
+// }
 
 
 // Utility functions
@@ -81,42 +126,27 @@ function cloudGetObj(key) {
 }
 
 
-// Game Tick
+// Local game
 function gameTick() {
     console.log("Tick!")
-
-    // Get game object
-    gameObj = cloudGetObj(currentGame)
-
-    // For each player get current metal. 
-    for (let i = 0; i < gameObj.players.length; i++) {
-        gameObj.players[i].metal += 10
-    }
-
-    cloudSave(currentGame, gameObj)
-    // Set new metal value based on tick
-
-    updateWebInterface(currentPlayer)
-
+    play(currentPlayer)
 }
-
 
 // Player webinterface
 function updateWebInterface(playerName) {
+    const playerObj = cloudGetObj(playerName)
 
-    // Get latest data from cloud
-    gameObj = cloudGetObj(currentGame)
-
-    // Get player object
-    for (let i = 0; i < gameObj.players.length; i++) {
-
-        if (gameObj.players[i].name === playerName) {
-
-            // update interface
-            playerNameEl.textContent = `Player: ${gameObj.players[i].name}`
-            statMetalEl.textContent = `Metal: ${gameObj.players[i].metal}`
-            statCrystalEl.textContent = `Crystal: ${gameObj.players[i].crystal}`
-            statEnergyEl.textContent = `Energy: ${gameObj.players[i].energy}`
-        }
-    }
+    // update interface
+    playerNameEl.textContent = `Player: ${playerName}`
+    statMetalEl.textContent = `Metal: ${playerObj.metal}`
+    statCrystalEl.textContent = `Crystal: ${playerObj.crystal}`
+    statEnergyEl.textContent = `Energy: ${playerObj.energy}`
 }
+
+
+function play(playerName) {
+    resourceTick(currentPlayer)
+    updateWebInterface(playerName)
+}
+
+play(currentPlayer)
